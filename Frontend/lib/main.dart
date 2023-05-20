12b2253117886d606b3f040f1fcf313d5f315807
AlwaysFighting <zosejiri@gmail.com>
2023-05-20 10:30:35 +0900

    Feat: app 파일을 로그인 폴더로 이동

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'home/views/app.dart';
+import 'login/views/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
